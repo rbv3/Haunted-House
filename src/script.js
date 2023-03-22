e7c48b8efe722fb90a42f378b68251228217b01d
Rodrigo Valenca <rvalenca@amazon.com>
2023-03-21 23:25:05 -0300

    add shadows to scene

--- src/script.js
+++ src/script.js
@@ -154,6 +154,8 @@ for(let i=0; i<50; i++) {
     grave.rotation.y = (Math.random() - 0.5) * 0.4
     grave.position.set(x, 0.8*0.4, z)
 
+    grave.castShadow = true
+
     graves.add(grave)
 }
 
@@ -192,10 +194,19 @@ gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(moonLight)
 
-const doorLight = new THREE.PointLight('#ff7d46', 1, 7)
+const doorLight = new THREE.PointLight('#ff7d46', 1, 8)
 doorLight.position.set(0, 2.2, 2.7)
 house.add(doorLight)
 
+/*
+ Ghosts
+*/
+const ghost1 = new THREE.PointLight('#ff00ff', 2, 3)
+scene.add(ghost1)
+const ghost2 = new THREE.PointLight('#00ffff', 2, 3)
+scene.add(ghost2)
+const ghost3 = new THREE.PointLight('#ffff00', 2, 3)
+scene.add(ghost3)
 /**
  * Sizes
  */
@@ -243,6 +254,41 @@ renderer.setClearColor('#262837')
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+/*
+* Shadow
+*/
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+moonLight.castShadow = true
+doorLight.castShadow = true
+ghost1.castShadow = true
+ghost2.castShadow = true
+ghost3.castShadow = true
+
+walls.castShadow = true
+walls.receiveShadow = true
+bush1.castShadow = true
+bush2.castShadow = true
+bush3.castShadow = true
+bush4.castShadow = true
+floor.receiveShadow = true
+
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 7
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 7
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 7
 /**
  * Animate
  */
@@ -252,6 +298,21 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // update ghost
+    const ghost1Angle = elapsedTime * 0.5
+    ghost1.position.x = Math.cos(ghost1Angle) * 4
+    ghost1.position.z = Math.sin(ghost1Angle) * 4
+    ghost1.position.y = Math.sin(elapsedTime * 3)
+
+    const ghost2Angle = -elapsedTime * 0.33
+    ghost2.position.x = Math.cos(ghost2Angle) * 5
+    ghost2.position.z = Math.sin(ghost2Angle) * 5
+    ghost2.position.y = Math.sin(elapsedTime * 2) + Math.sin(elapsedTime * 4)
+
+    const ghost3Angle = elapsedTime * 0.18
+    ghost3.position.x = Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32))
+    ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5))
+    ghost3.position.y = Math.sin(elapsedTime * 6)
     // Update controls
     controls.update()
 
